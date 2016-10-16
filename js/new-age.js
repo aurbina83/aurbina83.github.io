59ec5ce8395762e69312e44e44b8376c4c544fd9
Anthony Urbina <aurbina.metro@gmail.com>
2016-10-16 18:04:34 -0500

    added facebook assets

---
+++ js/new-age.js
@@ -0,0 +1,38 @@
+(function($) {
+    "use strict"; // Start of use strict
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 100
+    });
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+    // Offset for Main Navigation
+    $('#mainNav').affix({
+        offset: {
+            top: 50
+        }
+    })
+
+    // $('#fb').click(function() {
+    //     document.location = "fb://profile/1086625011361427";
+    //
+    //     setTimeOut(function() {
+    //         document.location = "https://www.facebook.com/veteranconnectco"
+    //     }, 300);
+    // })
+})(jQuery); // End of use strict
